242fc8822dcc018a90b463a4e890df8c1b603052
Rohan <robait18@gmail.com>
2023-07-28 21:15:13 +0530

    Hackerranks

--- src/hackerrank/ninth.java
+++ src/hackerrank/ninth.java
@@ -1,2 +1,15 @@
-package hackerrank;public class ninth {
+package hackerrank;
+import java.util.*;
+
+public class ninth {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = 1;
+        while(sc.hasNext()){
+            System.out.println(i+" "+sc.nextLine());
+            i++;
+
+        }
+        sc.close();
+    }
 }
